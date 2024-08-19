ddac2fd0819404760db1be57116df0a6d4f8045d
kamaleiah <klmnop.558@gmail.com>
2024-08-19 20:28:52 +0800

    change commit

--- src/index.js
+++ src/index.js
@@ -3,16 +3,13 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { ContextProvider } from './context/appContext';
-import { HashRouter } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <HashRouter>
     <ContextProvider>
       <App />
     </ContextProvider>
-    </HashRouter>
   </React.StrictMode>
 );
 
